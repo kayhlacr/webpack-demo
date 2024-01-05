632e27bd8da432b82c3b654416390240ad3323e4
Patrick Thesken <patrick@student.dev-island.org>
2024-01-04 20:29:12 -1000

    configured asset management modules.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import _ from "lodash";
+import "./style.css";
+import Icon from "./icons/mountain.jpg";
+import Data from "./data.xml";
+import Notes from "./data.csv";
+import data from "./sample.json";
 
 function component() {
   const element = document.createElement("div");
 
   //Lodash, now imported by this script
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.classList.add("hello");
+
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
+
+  console.log(Data);
+  console.log(Notes);
+
+  console.log("json stuff: ", data.glossary.title);
 
   return element;
 }
