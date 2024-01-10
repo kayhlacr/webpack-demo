3f2ff4b0088c00f0269c35665fb7fde5796a5266
Kayhlacr <kayhla@student.dev-island.org>
2024-01-09 19:20:20 -1000

    output management & using source maps

--- src/index.js
+++ src/index.js
@@ -4,12 +4,18 @@ import Icon from "./icons/mountain.jpg";
 import Data from "./data.xml";
 import Notes from "./data.csv";
 import data from "./sample.json";
+import printMe from "./print.js";
 
 function component() {
   const element = document.createElement("div");
+  const btn = document.createElement("button");
 
   //Lodash, now imported by this script
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+  btn.innerHTML = "Click me and check the console!";
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
   element.classList.add("hello");
 
   const myIcon = new Image();
